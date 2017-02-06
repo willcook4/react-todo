c2cb9ab9f7e84bbd070227e535f760524714ce02
Will Cook <hello@doubleucook.com>
2017-02-06 21:51:37 +0000

    added to the todolist test

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,9 +5,25 @@ var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 describe('TodoList', () => {
   it('should exist', () => {
     expect(TodoList).toExist();
+  });
+
+  it('should render one Todo Component for each todo item', () =>{
+    var todos = [{
+      id: 1,
+      text: 'do something'
+      }, {
+      id: 2,
+      text: 'anothe thing'
+    }];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+    expect(todosComponents.length).toBe(todos.length);
+
   });
 });
