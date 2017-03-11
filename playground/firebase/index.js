cc57ec8987c4dc40df0f2986e770169f40ab1214
Will Cook <hello@doubleucook.com>
2017-03-11 10:35:07 +0000

    firebase playground files

--- playground/firebase/index.js
+++ playground/firebase/index.js
@@ -13,9 +13,21 @@ firebase.initializeApp(config);
 
 
 var firebaseRef = firebase.database().ref();
+// Firebase Actions:
+
+// CREATE firebaseRef.set({ -> Set wipes the previous values
+// CREATE firebaseRef.child('user').set({ sets a child
+// READ firebaseRef.once('value').then((snapshot) => { console.log('Got the entire database here: ', snapshot.val()); });
+// READ firebaseRef.child('user').once('value').then((snapshot) => { console.log('Got user data', snapshot.val()); }); // Subset of data retrieved
+// UPDATE firebaseRef.update() // Modify data on the first level not nested properties
+//    // multipath updates 'path': newValue will do nested
+// DELETE firebaseRef.remove() -> Removes everything!
+// DELETE firebaseRef.child('app/name').remove(); -> Removes the child
+// DELETE firebaseRef.child('app').update({ name: null});  // Any data set to null will be reomved from firebase, same as remove
+
+
 
 firebaseRef.set({
-
   app: {
     name: 'Soemthing Silly',
     version: '0.1.1'
@@ -25,20 +37,80 @@ firebaseRef.set({
     name: 'Bill',
     age: 54
   }
-}).then(()=>{
-  console.log('Set Worked');
-}, (error)=>{
-  console.log('Set Failed: ', error);
 });
 
-// firebaseRef.set({                  // Set Wipes the previous values
-//   appName: 'Todo Application'
+// firebaseRef.update({
+//   isRunning: false,
+//   'app/name': 'Todo Application' // multipath updates 'path': newValue
 // });
 
-firebaseRef.child('user').set({ // Also returns a promise
-  name: 'Mike'
+// This is the same as the block above.
+// firebaseRef.child('app').update({
+//   name: 'Todo Application'
+// }).then(() => {
+//   console.log('update worked');
+// }, (e) => {
+//   console.log('Update Failed', e);
+// });
+
+// Exercise 1
+// firebaseRef.update({
+//   'app/name': 'New name',
+//   'user/name': 'Timmy'
+// });
+
+// Exercise 2
+// firebaseRef.child('app').update({
+//   name: 'Another name'
+// }).then(()=>{
+//   firebaseRef.child('user').update({
+//     name: 'Judy'
+//   });
+// });
+
+// firebaseRef.child('app/name').remove();
+//
+// firebaseRef.child('app').update({
+//   version: '2.0',
+//   name: null   // Any data set to null will be reomved from firebase, same as remove
+// });
+
+// Exercise 3
+// firebaseRef.update({
+//   isRunning: null
+// });
+//
+// // Exercise 4
+// firebaseRef.child('user/age').remove();
+
+// firebaseRef.once('value').then((snapshot) => {
+//   console.log('Got entire database', snapshot.val());
+// }, (error)=>{
+//   console.log('unabe to fetch value', error);
+// });
+//
+// firebaseRef.child('user').once('value').then((snapshot) => {
+//   console.log('Got user data', snapshot.key, snapshot.val());
+// }, (error)=>{
+//   console.log('unabe to fetch value', error);
+// });
+//
+// firebaseRef.on('value', (snapshot) => { // Turns on the Listener when this value changes
+//   console.log('Got value', snapshot.val());
+// });
+//
+// firebaseRef.off(); // Turns off all the event Listeners
+//
+// firebaseRef.update({isRunning: false});
+
+firebaseRef.child('user').on('value', (snapshot) => {
+  console.log('Updated the user name:', snapshot.val());
 });
 
-firebaseRef.child('app').set({
-  name: 'Todo App'
+firebaseRef.child('user').update({
+  name: 'Kate'
+});
+
+firebaseRef.child('user').update({
+  name: 'Something completly new'
 });
